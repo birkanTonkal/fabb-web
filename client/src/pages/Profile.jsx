81ac97f1a655f40ac9f88bd9ea5560724847f2cb
Kamil Birkan Tonkal <tonkalkamilbirkan@gmail.com>
2023-06-01 19:59:29 +0300

    profile data fetch

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useSelector } from "react-redux";
 
 function Profile() {
-    
+    const authState = useSelector((state) => state.auth);
+    const userData = authState.user
+    console.log(userData)
     return (
         <div></div>
     )
